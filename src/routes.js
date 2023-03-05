31984bcbe71fc016e214716f0edc1258bd08ae4a
Rainer Aas <raineraas2@gmail.com>
2023-03-05 11:18:56 +0200

    asd

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.post('/images', (req, res) => {
   res.send('Hello world');
 });
 
-// - real stuff below - TODO: remove this comment later
-
-router.post('/images', (req, res) => { // pildi ülesse laadimine
+router.get('/images', (req, res) => {
   res.send('Hello world');
 });
 
-router.get('/images', (req, res) => { // piltide päring
-  res.send('Hello world');
-});
-
-router.delete('/images/:imageId', (req, res) => { // pildi kustutamine
+router.delete('/images/:imageId', (req, res) => {
   res.send('Hello world');
 });
 
