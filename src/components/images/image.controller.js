073e18ff462a7681636d57f51865f4457fb23cad
Rainer Aas <raineraas2@gmail.com>
2023-03-06 12:17:04 +0200

    Jimp testing

--- src/components/images/image.controller.js
+++ src/components/images/image.controller.js
@@ -1,7 +1,14 @@
-export async function uploadImage(req, res) {
-  console.log(req);
+import Jimp from 'jimp';
 
-  res.send('Upload image');
+export async function uploadImage(req, res) {
+  console.time();
+  const resizedBuffer = await Jimp.read(req.file.buffer).then((img) => img.resize(150, 150)
+    .getBufferAsync(req.file.mimetype));
+  console.timeEnd();
+
+  const b64 = Buffer.from(resizedBuffer).toString('base64');
+
+  res.send({ b64, mimetype: req.file.mimetype });
 }
 
 export async function getImages(req, res) {
