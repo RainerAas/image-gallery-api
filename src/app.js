e567346c78b5022975f5757f2e0bf2a15617743b
Rainer Aas <raineraas2@gmail.com>
2023-03-20 01:24:37 +0200

    Add error handler

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import routes from './routes';
 
+const errorHandler = (err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res.status(500).send({ error: err.message });
+};
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
+app.use(errorHandler);
 
 export default app;
